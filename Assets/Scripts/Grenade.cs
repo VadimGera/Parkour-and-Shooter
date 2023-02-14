8e074b1d59de2186498eeed5c23b06c3dea786f8
VadimGera <vadimnvidia@gmail.com>
2023-02-14 23:41:48 +0300

    Add Grenade and Scripts Grenade

---
+++ Assets/Scripts/Grenade.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Cinemachine;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Grenade : MonoBehaviour
+    {
+        [SerializeField] private float delay = 3f;
+        [SerializeField] private float radius = 5f;
+        [SerializeField] private float force = 700f;
+        [SerializeField] private float damage = 80f;
+
+
+        public GameObject explosionEffect;
+
+        private float countdown;
+        private bool hasExploded = false;
+
+        void Start()
+        {
+            countdown = delay;
+        }
+
+        private void Update()
+        {
+            countdown -= Time.deltaTime;
+            if (countdown <= 0f && !hasExploded)
+            {
+                Explode();
+                hasExploded = true;
+            }
+        }
+
+        private void Explode()
+        {
+            Instantiate(explosionEffect, transform.position, transform.rotation);
+
+            Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+            foreach (Collider nearbyObject in colliders)
+            {
+                Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddExplosionForce(force, transform.position, radius);
+                }
+
+                var destructible = nearbyObject.GetComponent<DestructibleObject>();
+                if (destructible != null)
+                {
+                    destructible.ReceiveDamage(damage);
+                }
+            }
+            
+            
+            Destroy(gameObject);
+        }
+        
+        
+        
+    }
+}
---
+++ Assets/Scripts/GrenadeThrow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GrenadeThrow : MonoBehaviour
+    {
+        [SerializeField] private float throwForce = 50f;
+        [SerializeField] private GameObject grenadePrefab;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                GrenadeThrowForward();
+            }
+        }
+
+
+        private void GrenadeThrowForward()
+        {
+            GameObject grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
+
+            Rigidbody rb = grenade.GetComponent<Rigidbody>();
+            
+            rb.AddForce(transform.forward * throwForce, ForceMode.VelocityChange);
+        }
+    }
+
+
+}
